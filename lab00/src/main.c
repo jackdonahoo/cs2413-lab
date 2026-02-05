c164eb8131cacc9cab16bcebfaef3ade4f25b796
Jack Donahoo <jackdonahoo@gmail.com>
2026-02-05 15:03:18 -0600

    Finish lab00

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -4,9 +4,9 @@ int main(void) {
     printf("CS2413 Lab00 Starter Demo\n");
 
     // TODO: Replace the placeholder information below
-    printf("Name: Shawn\n");
-    printf("R#: 11714613\n");
-    printf("Section: 002\n");
+    printf("Name: Jack\n");
+    printf("R#: 12045731\n");
+    printf("Section: 506\n");
     printf("Message: I can use GitHub for CS2413 labs!\n");
 
     return 0;
