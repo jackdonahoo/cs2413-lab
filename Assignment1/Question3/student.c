570921a8a2fcbe07b6ec314c82e2fa19cd6e7070
jackdonahoo <Jackdonahoo@gmail.com>
2026-03-04 11:27:58 -0600

    Assignment 1 Check

--- Assignment1/Question3/student.c
+++ Assignment1/Question3/student.c
@@ -26,8 +26,22 @@
 //Output parameter (returnSize): set *returnSize to the number of digits in the returned array.
 
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
-    // TODO: implement
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            *returnSize = digitsSize;
+            int* out = (int*)malloc(digitsSize * sizeof(int));
+            for (int j = 0; j < digitsSize; j++) out[j] = digits[j];
+            return out;
+        }
+        digits[i] = 0;
+    }
 
-    
+    /* All digits were 9 (e.g. 999 -> 1000) */
+    *returnSize = digitsSize + 1;
+    int* out = (int*)malloc((digitsSize + 1) * sizeof(int));
+    out[0] = 1;
+    for (int i = 1; i <= digitsSize; i++) out[i] = 0;
+    return out;
 }
 
